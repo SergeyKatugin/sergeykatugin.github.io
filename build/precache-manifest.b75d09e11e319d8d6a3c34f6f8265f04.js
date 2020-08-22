a195fb70d986650e30ef1e20ed7cbe4d8813fd8f
Sergey Katugin <svkatugin@tsum.ru>
2020-08-22 10:26:47 +0300

    Build

---
+++ build/precache-manifest.b75d09e11e319d8d6a3c34f6f8265f04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be57fcffd94678f3e2eeb482603bc1d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b33a52d6505d895c77",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d0a4574301aa22d9d4a",
+    "url": "/static/js/2.08b1a029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08b1a029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b33a52d6505d895c77",
+    "url": "/static/js/main.7aed3491.chunk.js"
+  },
+  {
+    "revision": "40f818bd6321760b04a6",
+    "url": "/static/js/runtime-main.e931f33c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
